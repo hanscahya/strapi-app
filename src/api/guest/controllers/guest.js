cf9641ebeeb827c87124c0bf97a4b37bb42d458b
Hans Cahya <hanscahya@users.noreply.github.com>
2022-01-16 15:13:39 +0700

    Revmoe console

--- src/api/guest/controllers/guest.js
+++ src/api/guest/controllers/guest.js
@@ -8,16 +8,12 @@ const { createCoreController } = require('@strapi/strapi').factories;
 
 module.exports = createCoreController('api::guest.guest', ({ strapi }) => ({
   async create(ctx) {
-    console.log(ctx.request.body)
-
     const created = await strapi.services["api::guest.guest"].create(ctx.request.body);
     const updated = await strapi.services["api::guest.guest"].update(created.id, {
       data: {
         link: `https://nadyahans.family?id=${created.id}&to=${encodeURI(created.fullName)}`,
       }
     });
-
-    console.log(updated);
     ctx.send(updated);
   },
 }));
